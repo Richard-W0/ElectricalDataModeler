277ed25a89fbeca19f0908d6c2d5c4fa1422cc2f
Richard-W0 <rikuwong@hotmail.fi>
2024-11-04 13:08:20 +0200

    files

---
+++ HTTavoite.py
@@ -0,0 +1,14 @@
+######################################################################
+# CT60A0203 Ohjelmoinnin perusteet
+# Tekijä: Richard Wong
+# Opiskelijanumero: 
+# Päivämäärä: 30.10.2024
+# Kurssin oppimateriaalien lisäksi työhön ovat vaikuttaneet seuraavat
+# lähteet ja henkilöt, ja se näkyy tehtävässä seuraavalla tavalla:
+#
+# Mahdollisen vilppiselvityksen varalta vakuutan, että olen tehnyt itse
+# tämän tehtävän ja vain yllä mainitut henkilöt sekä lähteet ovat
+# vaikuttaneet siihen yllä mainituilla tavoilla.
+######################################################################
+# Tehtävä Harjoitustyö
+# eof
